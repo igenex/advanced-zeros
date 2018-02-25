43a5efd2dd609e0572d547e6a9355febe29402cf
Eugene <igenexxx@gmail.com>
2018-02-25 20:18:39 +0300

    79 passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-module.exports = function getZerosCount(number, base) {
+module.exports =
+function getZerosCount(number, base) {
   // your implementation
-}
+  function findDivisor(num) {
+    "use strict";
+    let i = 1;
+
+    function getNum(num) {
+      while ((num % i)) {
+        i++;
+      }
+      num /= i;
+      if (!(num % i)) {
+        if(i === 1) i++;
+        getNum(num);
+      } else {
+        if (num !== 1) {
+          i++;
+          getNum(num);
+        }
+      }
+      return i;
+    }
+
+    getNum(num);
+
+    return i;
+  }
+
+  let divisor = findDivisor(base);
+
+  function findFactorialZeros(number, div) {
+    "use strict";
+    let result = 0;
+    while (Math.floor(number) > 0) {
+      number /= div;
+      result += Math.floor(number);
+    }
+    return result;
+  }
+
+  return findFactorialZeros(number, divisor);
+};
+
+
